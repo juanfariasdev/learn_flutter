1ffce399d3ed856cdc635fd184ef870c1034e32c
Juan Pablo Farias <juan.pablo@arcca.io>
2024-11-20 21:17:58 -0300

    add core structure and initial pages for the app

---
+++ lib/pages/home/home_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: const Center(
+          child: Text("Home Page"),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pages/splash/splash_page.dart
@@ -0,0 +1,48 @@
+import 'package:learn_flutter/core/core.dart';
+import 'package:flutter/material.dart';
+import 'package:learn_flutter/pages/home/home_page.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
+    _redirectToGetStarted();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: SizedBox(
+          height: 150,
+          child: Image.asset(
+            AppImages.logo,
+          ), // Usando o LogoWidget para exibir o logotipo,
+        ),
+      ),
+    );
+  }
+
+  Future<void> _redirectToGetStarted() async {
+    await Future.delayed(const Duration(seconds: 2));
+    if (mounted) {
+      _navigateToGetStartedPage(); // Só navega se o State ainda estiver montado
+    }
+  }
+
+  void _navigateToGetStartedPage() {
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(
+        builder: (context) => const HomePage(), // Navega para a página de slide
+      ),
+    );
+  }
+}
