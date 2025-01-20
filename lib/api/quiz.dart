2c6aaff2bd51bffa7f82e05e8c6d02ff1df1303e
Juan Pablo Farias <juanpablo.profissional.a.f@gmail.com>
2025-01-20 14:39:58 -0300

    implement server

--- lib/api/quiz.dart
+++ lib/api/quiz.dart
@@ -1,13 +1,29 @@
-import 'package:flutter/material.dart';
-import 'package:learn_flutter/models/quiz_model.dart';
+import 'dart:convert';
 
-import 'json.dart'; // Seu arquivo json.dart
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:learn_flutter/models/quiz_model.dart';
 
 class QuizApi {
   Future<List<QuizModel>> getQuizzes() async {
     WidgetsFlutterBinding.ensureInitialized();
-    final quizData = await getItems('database/quiz.json');
-    final list = quizData.map((e) => QuizModel.fromMap(e)).toList();
-    return list;
+    final quizDataResponse =
+        await http.get(Uri.parse("http://10.0.2.2:3000/api/quiz"));
+
+    if (quizDataResponse.statusCode == 200) {
+      // Garantir que a resposta esteja na codificação UTF-8
+      final quizData = utf8.decode(
+          quizDataResponse.bodyBytes); // Decodificando explicitamente com UTF-8
+      final quizList = jsonDecode(quizData) as List<dynamic>;
+
+      // Converta cada elemento para QuizModel e transforme o Iterable em uma lista.
+      final List<QuizModel> list =
+          quizList.map((e) => QuizModel.fromMap(e)).toList();
+
+      return list;
+    }
+
+    // Retorne uma lista vazia em caso de falha
+    return [];
   }
 }
--- lib/api/user.dart
+++ lib/api/user.dart
@@ -1,12 +1,23 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'package:learn_flutter/models/user_model.dart';
 
-import 'json.dart'; // Seu arquivo json.dart
-
 class UserApi {
-  Future<UserModel> getMyUser() async {
+  Future<UserModel?> getMyUser() async {
     WidgetsFlutterBinding.ensureInitialized();
-    final userData = await getItem('database/user.json');
-    return UserModel.fromMap(userData);
+    final userDataResponse =
+        await http.get(Uri.parse("http://10.0.2.2:3000/api/user/me"));
+
+    if (userDataResponse.statusCode == 200) {
+      final userData = utf8.decode(
+          userDataResponse.bodyBytes); // Decodificando explicitamente com UTF-8
+      final user = jsonDecode(userData) as Map<String, dynamic>;
+
+      return UserModel.fromMap(user);
+    }
+
+    return null;
   }
 }
